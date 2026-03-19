1a968e4cd4697d5db3718508eae637d182be4d93
Willy Saronamihardja <wsaronam@gmail.com>
2026-03-19 01:32:48 -0700

    updates
    - working on backend for calling ai, might switch ai sources, current is giving me problems

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,38 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
 
 
 
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
+genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
+model = genai.GenerativeModel('gemini-2.0-flash')
 
 
 
 @app.route('/ping')
 def ping():
     return {'status': 'ok'}
+
+
+@app.route("/generate-map", methods=["POST"])
+def generate_map():
+    data = request.json
+    theme = data.get('theme', 'fantasy dungeon')
+
+    prompt=f"""
+    Please generate a dungeon map description for a {theme} setting.
+     """
+    
+    response = model.generate_content(prompt)
+    return response
 
 
 
@@ -23,5 +43,5 @@ if __name__ == "__main__":
 
 
 # terminal cmd to run
-# venv/bin/activate
+# venv\Scripts\activate
 # python app.py
